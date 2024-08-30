7073d66789bc159c09e89fae2e277714d859d326
tasoWEB <xxxonz0730@gmail.com>
2024-08-30 18:10:33 +0900

    チャプター８

---
+++ src/app/about/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h2>about</h2>
+    </>
+  );
+}
---
+++ src/app/work/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <h2>work</h2>
+    </>
+  );
+}
