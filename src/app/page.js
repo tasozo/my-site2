0ac319651239b4468012926e0610089cf7f06724
tasoWEB <xxxonz0730@gmail.com>
2024-09-03 20:38:08 +0900

    ライセンス表示移動作戦①

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,7 @@
-
-import Link from 'next/link'
-
 export default function Home() {
   return (
-    <>
-      <div>
-        <h2>アイコンのライセンス</h2>
-        <br />
-        <p>このサイトで使用しているアイコンは、「Font Awesome」のFreeプランを利用しています。</p>
-        <p>Font Awesomeのライセンスについては、以下のリンクを参照してください。</p>
-        <br />
-        <Link href='https://fontawesome.com/license/free' className='text-blue-500'>
-          Font Awesome Free License
-        </Link>
-      </div>
-    </>
-  )
+    <main className="flex flex-col items-center justify-between p-24">
+      Hello world
+    </main>
+  );
 }
